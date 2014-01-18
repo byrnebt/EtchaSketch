3bf48a6cdd17f1ceddb2182f9a7b2e39a1fb949f
Brendan Byrne <byrnebt@gmail.com>
2014-01-17 18:11:34 -0800

    added keypresses, next step resizing

--- Etch.js
+++ Etch.js
@@ -1,36 +1,47 @@
 $(document).ready(function(){
 
-	var stay_var = 1;
+	var cursor_var = 1;
+	var cursor_on = 1;
 
 	create_board(32, 32);
-
 	$(".board").mouseenter(function() {
-		if (stay_var == 1)
-		{
-			$(this).toggleClass("fill");
+		if (cursor_on == 1) {
+			if (cursor_var == 1)
+			{
+				$(this).addClass("fill");
+			}
+			if (cursor_var == -1)
+			{
+				$(this).removeClass("fill");
+			}
 		}
-		if (stay_var == -1)
-		{
-			$(this).addClass("fill");
-		}
-	}); //base fill function
+	}); //fill function
 
+	$(document).bind('keyup', function(keypress) {
+		if (keypress.which == 69) {//user pressed e
+			cursor_var *= -1; //toggle erasing
+		}
+		if (keypress.which == 67) { //user pressed c
+			$(".fill").removeClass("fill");
+		}
+		if (keypress.which == 84) { //user pressed t
+			cursor_on *= -1;
+			console.log("pressed t");
+		}
+	});
 
 	$("#clear").click(function() {
 		$(".fill").removeClass("fill");
 	});//clear function
 
-	$("#stay").click(function() {
-		stay_var *= -1;
+	$("#eraser").click(function() {
+		cursor_var *= -1;
 	});
 });
 
-function create_board (i, j) 
-{
-	for (var i=0; i < 32; i++)
-	{
-		for (var j=0; j < 32; j++)
-		{
+function create_board (i, j) {
+	for (var i=0; i < 32; i++) {
+		for (var j=0; j < 32; j++) {
 			$('#board').append("<div class='height_"+ i +" width_"+ j +" board left'></div>");
 		}
 	}
